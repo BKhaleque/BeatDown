76f0a452025fa454fc27aa7732fb6344436ee670
BobbyKhaleque <Bobby.Khaleque@hotmail.com>
2020-11-03 12:27:52 +0000

    added note and activator prefab and scripting

---
+++ Beat Down/Assets/Scripts/Activator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activator : MonoBehaviour
+{
+
+    public KeyCode key;
+    bool active = false;
+    GameObject note;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(key) && active){
+            Destroy(note);
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col){
+        active = true;
+        if(col.gameObject.tag=="Note")
+            note= col.gameObject;
+    }
+
+    void OnTriggerExit2D(Collider2D col){
+        active = false;
+    }
+}
