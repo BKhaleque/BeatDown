921189e831c5cb9c6925dbcf89cff7d4ca15cca3
unknown <dmactte1995@gmail.com>
2020-11-04 16:12:01 +0000

    notecreator1

---
+++ Beat Down/Assets/Notecreator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notecreator : MonoBehaviour
+{
+    int[] timeline={1,1,1};
+    public GameObject n;
+    int time = 1;
+    int i = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        create();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void create()
+    {
+        Instantiate(n,transform.position,Quaternion.identity);
+        if(i >= timeline.Length)
+        {
+            return;
+        }
+        time = timeline[i];
+        i++;
+        Invoke("create", time);
+    }
+}
